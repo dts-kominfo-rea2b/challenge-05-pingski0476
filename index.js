2a3a0412c2b4912920cf8f4c47b068596f44a97c
Afrillian Ramadhan <pingski.ramadhan@gmail.com>
2022-06-04 21:27:05 +0700

    adding sort callback func

--- index.js
+++ index.js
@@ -6,15 +6,26 @@ const names = ["Halo", "Angel", "Nyoman", "Ketut", "Aisyah"];
 // "1. Nama"
 // "2. Nama"
 // ...
-const sorter = null;
+const sorter = (nama, sortfunc) => {
+  let sorted = sortfunc(nama);
+  let arr=[];
+  for(let i=0;i<=sorted.length-1;i++){
+    arr[i]= (i+1)+". "+ sorted[i];
+  }
+  return arr;
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara ascending
 // mengembalikan array yang sudah diurutkan
-const sortAscending = null;
+const sortAscending = (nama) => {
+  return nama.sort();
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara descending
 // mengembalikan array yang sudah diurutkan
-const sortDescending = null;
+const sortDescending = (nama) => {
+  return nama.reverse();
+};
 
 // ! JANGAN DIMODIFIKASI
 (function main() {
